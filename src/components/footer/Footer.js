43b60f5d9d1d72292daad0b0d6413960720d1edf
Zouss28 <alanjonse2@gmail.com>
2024-01-05 04:12:20 +0200

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,12 +10,12 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by DeveloperFolio Team")}
+          {emoji("Made By Iyakaremye Alain")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
-          <a href="https://github.com/saadpasta/developerFolio">
-            developerFolio
+          <a href="https://github.com/Zouss28/github-portfolio">
+            Zouss
           </a>
         </p>
       </div>
